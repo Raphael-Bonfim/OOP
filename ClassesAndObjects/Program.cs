34dda2e999430254bed2a17dab1f7a533f39d9ad
Raphael Bonfim <raphael.bonfim.fer@gmail.com>
2023-10-13 00:39:55 -0300

    ClassesAndObjects first commit.

---
+++ ClassesAndObjects/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ClassesAndObjects;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+     /*
+        Objects: An object is an instance of a class.
+        It's a concrete realization of the class blueprint 
+        and contains data (attributes) and methods (functions) that operate on that data.
+     */
+        var payment = new Payment(10.56, DateTime.Today);
+
+        Console.WriteLine(payment.Amount);
+        Console.WriteLine(payment.DueDate);
+    }
+
+
+    /*
+        Classes: A class is a blueprint or template for creating objects. 
+        It defines the structure and behavior of objects of that class.
+        For example, a "Payment" class could define the properties and methods that all Payments share.
+    */
+    class Payment
+    {
+        //Constructor
+        public Payment(double amount, DateTime duedate) { 
+        
+            Amount = amount;
+            DueDate = duedate;
+        }
+
+        //Properties
+        public DateTime DueDate { get; private set; }
+        
+        public double Amount { get; private set; }
+
+        //Methods
+        public void ProcessPayment() {}
+
+    }
+}
