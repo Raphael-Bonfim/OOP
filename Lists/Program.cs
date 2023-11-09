c3b16271facff02764ed6724d957b18a4f8fef3d
Raphael Bonfim <raphael.bonfim.fer@gmail.com>
2023-11-08 21:45:41 -0300

    Lists commit

---
+++ Lists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IList<Payment> payments = new List<Payment>();
+
+        payments.Add(new Payment(1));
+        payments.Add(new Payment(2));
+        payments.Add(new Payment(3));
+        payments.Add(new Payment(4));
+        payments.Add(new Payment(5));
+
+        foreach (var item in payments) 
+        { 
+            Console.WriteLine(item.Id);
+        }
+
+        var payment = payments.First(x => x.Id == 3);
+
+        payments.Remove(payment);
+
+        foreach (var item in payments)
+        {
+            Console.WriteLine(item.Id);
+        }
+
+        var exists = payments.Any(x => x.Id == 3);
+        Console.WriteLine(exists);
+    }
+
+    public class Payment
+    {
+        public int Id { get; set; }
+        public Payment(int id)
+        {
+            Id = id;
+        }
+    }
+}
